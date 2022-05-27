4c4c14c6849f5dc56df3733def3697167b26f0d2
zhouqi <zq@differsoft.com>
2022-05-27 16:09:19 +0800

    refactor: change comment

--- packages/react-reconciler/src/ReactWorkTags.ts
+++ packages/react-reconciler/src/ReactWorkTags.ts
@@ -2,12 +2,12 @@
  * @Author: Zhouqi
  * @Date: 2022-05-16 21:39:57
  * @LastEditors: Zhouqi
- * @LastEditTime: 2022-05-27 15:15:19
+ * @LastEditTime: 2022-05-27 16:07:10
  */
 // 当前应用的根节点
 export const HostRoot = 3;
 // Function组件标记
 export const IndeterminateComponent = 2; // Before we know whether it is function or class
 export const FunctionComponent = 0;
-// 普通元素类的标记
+// 原生dom元素对应的fiber节点类型
 export const HostComponent = 5;
