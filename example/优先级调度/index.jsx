64a8376b0d8ca481b3119d0bc36bce3e88ecc861
zhouqi <zq@differsoft.com>
2022-06-26 20:53:16 +0800

    feat:test demo

--- example/优先级调度/index.jsx
+++ example/优先级调度/index.jsx
@@ -2,34 +2,31 @@
  * @Author: Zhouqi
  * @Date: 2022-05-31 16:21:54
  * @LastEditors: Zhouqi
- * @LastEditTime: 2022-06-22 20:33:08
+ * @LastEditTime: 2022-06-26 20:52:22
  */
-const { useState } = React;
+const { useState, useEffect } = React;
 
 const App = () => {
   const [num, setNum] = useState(0);
+  useEffect(() => {
+    const btn = document.getElementById("btn1");
+    setTimeout(() => {
+      setNum((num) => num + 1);
+    }, 1000);
+    setTimeout(() => {
+      btn.click();
+    }, 1010);
+  }, []);
   return (
     <div className="red">
       <input type="text" />
       <button
         id="btn1"
         onClick={() => {
-          setNum((num) => num + 1);
+          setNum((num) => num + 2);
         }}
       ></button>
-      <button
-        onClick={() => {
-          const btn = document.getElementById("btn1");
-          setTimeout(() => {
-            setNum((num) => num + 2);
-          }, 1000);
-          setTimeout(() => {
-            btn.click();
-          }, 1010);
-        }}
-      >
-        更新
-      </button>
+      <button onClick={() => {}}>更新</button>
       {Array.from(new Array(100000)).map((item, i) => (
         <h1 key={i}>{num}</h1>
       ))}
