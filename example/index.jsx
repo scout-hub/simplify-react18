6c2b83174a1edaa482a0323a700cddd76e1b1640
zhouqi <zq@differsoft.com>
2022-06-30 16:50:25 +0800

    feat:setState

--- example/index.jsx
+++ example/index.jsx
@@ -2,7 +2,7 @@
  * @Author: Zhouqi
  * @Date: 2022-05-31 16:21:54
  * @LastEditors: Zhouqi
- * @LastEditTime: 2022-06-30 16:46:15
+ * @LastEditTime: 2022-06-30 16:48:56
  */
 const { Component, useState } = React;
 
@@ -14,11 +14,6 @@ class Child extends Component {
       num: 0,
     };
   }
-
-  // static getDerivedStateFromProps(nextProps, prevState) {
-  //   console.log("getDerivedStateFromProps");
-  //   return nextProps;
-  // }
 
   update = () => {
     // 只执行一次更新(批量处理)，因为优先级相同，后续的更新直接return了（ensureRootIsScheduled）
@@ -34,42 +29,13 @@ class Child extends Component {
 
     // 后一个setState的值依赖前一个
     this.setState((state, props) => {
-      return { num: state.num + props.num + 1 };
+      return { num: state.num + 1 };
+    });
+    this.setState((state, props) => {
+      console.log(state.num);
+      return { num: state.num + 1 };
     });
   };
-
-  componentWillMount() {
-    // 这里同步调用setState不会触发额外的渲染更新
-    this.setState({ num: 14 }, () => {
-      console.log("setState callback", this.state);
-    });
-  }
-
-  // componentWillReceiveProps(newProps) {
-  //   console.log("componentWillReceiveProps", newProps);
-  // }
-
-  // shouldComponentUpdate(newProps, newState) {
-  //   console.log("shouldComponentUpdate", newProps, newState);
-  //   return true;
-  // }
-
-  // componentDidMount() {
-  //   console.log("componentDidMount");
-  // }
-
-  // componentWillUpdate(newProps, newState) {
-  //   console.log("componentWillUpdate", newProps, newState);
-  // }
-
-  // getSnapshotBeforeUpdate(prevProps, prevState) {
-  //   console.log("getSnapshotBeforeUpdate", prevProps, prevState);
-  //   return {};
-  // }
-
-  // componentDidUpdate(prevProps, prevState, snapshot) {
-  //   console.log("componentDidUpdate", prevProps, prevState, snapshot);
-  // }
 
   render() {
     console.log("render");
